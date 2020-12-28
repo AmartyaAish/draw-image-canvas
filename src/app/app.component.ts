b25c84f2df4d3c50a969e254eccc518e6c8a08bc
Amartya Aishwarya <amartya@koverhoop.com>
2020-12-28 12:43:45 +0530

    draw image on  canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'create-polygon';
+  @ViewChild('myCanvas') myCanvas: ElementRef;
+  image = new Image();
+  constructor() {}
+
+  url: string;
+
+  selectFile(event: any): void {
+    if (event.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+        this.image.src = this.url;
+      };
+
+      let ctx: CanvasRenderingContext2D = this.myCanvas.nativeElement.getContext(
+        '2d'
+      );
+
+      this.image.onload = () => {
+        ctx.clearRect(
+          0,
+          0,
+          this.myCanvas.nativeElement.width,
+          this.myCanvas.nativeElement.height
+        );
+        // ctx.canvas.height =
+        // this.myCanvas.nativeElement.width = this.image.width;
+        // this.myCanvas.nativeElement.height = this.image.height;
+        ctx.drawImage(this.image, 0, 0, 1280, 720);
+      };
+    }
+  }
+
+  ngOnInit() {}
 }
