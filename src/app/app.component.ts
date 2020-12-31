235ecf633022ba1500347a734e4b01e0ba45c0e9
Amartya Aishwarya <amartya@koverhoop.com>
2020-12-31 17:09:07 +0530

    wip-polygon check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,9 +61,11 @@
 //   }
 // }
 
-//LAKSHAY CODE
+//LAKSHAY SIR CODE
 import { Component, OnInit } from '@angular/core';
 import { fabric } from 'fabric';
+// import 'fabric';
+// declare const fabric: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -202,6 +204,7 @@ export class AppComponent implements OnInit {
       let lastControl = poly.points.length - 1;
       poly.cornerStyle = 'circle';
       poly.cornerColor = 'rgba(0,0,255,0.5)';
+
       poly.controls = poly.points.reduce(function(acc, point, index) {
         acc['p' + index] = new fabric['Control']({
           positionHandler: polygonPositionHandler,
